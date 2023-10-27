052af8375b2db15c0b0fabd41564a38e7e9fe9e0
Nicram123 <141836446+Nicram123@users.noreply.github.com>
2023-10-27 12:29:11 +0200

    Add files via upload

---
+++ SQL_1.sql
@@ -0,0 +1,13 @@
+SELECT adres,nazwisko FROM pracownicy;
+
+SELECT data FROM godziny;
+
+SELECT DATE_PART('dow', data) AS dzien_tygodnia, DATE_PART('month', data) AS miesiac
+FROM godziny;
+
+
+SELECT * FROM pensje;
+
+
+
+
